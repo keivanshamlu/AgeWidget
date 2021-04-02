9553ef58051c5e6da8a8b31d29a3f5490b5860f0
Keivan Shamlu <keivan.shamlu.ks@gmail.com>
2021-04-02 16:13:19 +0430

    i added activity main as configuration activity to the widget , everything working fine with showing the age but i should get user data, so i start to adding hilt and stuff for starting implementing DI and after that architecture

--- app/src/main/java/com/shamlou/agewidget/MainActivity.kt
+++ app/src/main/java/com/shamlou/agewidget/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.shamlou.agewidget
 
+import android.app.Activity
+import android.appwidget.AppWidgetManager
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setResult(RESULT_CANCELED);
+        val mAppWidgetId = intent.extras?.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+
+
+        val resultValue = Intent()
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId)
+        setResult(Activity.RESULT_OK, resultValue)
+        finish()
 
     }
 }

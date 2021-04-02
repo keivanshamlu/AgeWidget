b6db0f3060b93b19471ca529878bb164c0a85788
Keivan Shamlu <keivan.shamlu.ks@gmail.com>
2021-04-02 17:10:53 +0430

    adding coroutines

--- app/src/main/java/com/shamlou/agewidget/ui/ViewModelMain.kt
+++ app/src/main/java/com/shamlou/agewidget/ui/ViewModelMain.kt
@@ -2,7 +2,8 @@ package com.shamlou.agewidget.ui
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class ViewModelMain : ViewModel() {
+class ViewModelMain @Inject constructor() : ViewModel() {
 }

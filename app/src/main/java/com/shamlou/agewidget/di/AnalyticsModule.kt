c3dbcca2f9b64e67a4b522a4e4931b572eba63e3
Keivan Shamlu <keivan.shamlu.ks@gmail.com>
2021-04-03 18:21:38 +0430

    create a room DB and provide it via Hilt dependency injection

---
+++ app/src/main/java/com/shamlou/agewidget/di/AnalyticsModule.kt
@@ -0,0 +1,26 @@
+package com.shamlou.agewidget.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.shamlou.agewidget.db.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AnalyticsModule {
+
+  @Provides
+  fun provideAnalyticsService(
+    @ApplicationContext applicationContext: Context
+  ): RoomDatabase {
+    return Room.databaseBuilder(
+      applicationContext,
+      AppDatabase::class.java, "database-name"
+    ).build()
+  }
+}

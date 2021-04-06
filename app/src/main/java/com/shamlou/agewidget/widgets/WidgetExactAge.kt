522c6866e853f32d27c56df2f398bb9135e67fac
keivanshamlu <keivan.shamlu.co@gmail.com>
2021-04-06 23:58:25 +0430

    handling widget

--- app/src/main/java/com/shamlou/agewidget/widgets/WidgetExactAge.kt
+++ app/src/main/java/com/shamlou/agewidget/widgets/WidgetExactAge.kt
@@ -7,58 +7,102 @@ import android.appwidget.AppWidgetProvider
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.os.SystemClock
+import android.view.View
 import android.widget.RemoteViews
 import com.shamlou.agewidget.R
+import com.shamlou.agewidget.base.BirthResource
+import com.shamlou.agewidget.domain.UserBirthDomain
 import com.shamlou.agewidget.manager.TimeManager
+import com.shamlou.agewidget.usecases.UseCaseBirthCheckUserBirthCache
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.Period
-import java.time.format.DateTimeFormatter
-import java.util.*
-import java.util.concurrent.TimeUnit
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 private const val ACTION_SCHEDULED_UPDATE = "com.shamlou.agewidget.SCHEDULED_UPDATE"
+
 @AndroidEntryPoint
 class WidgetExactAge : AppWidgetProvider() {
 
-    @Inject lateinit var timeManager: TimeManager
+    @Inject
+    lateinit var timeManager: TimeManager
+    @Inject
+    lateinit var useCaseBirthCheckUserBirthCache: UseCaseBirthCheckUserBirthCache
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
 
-        scheduleNextUpdate(context);
-        val remoteViews = RemoteViews(
+        scheduleNextUpdate(context)
+        var remoteViews = RemoteViews(
             context.packageName,
             R.layout.widget_exact_age
         )
-        val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
-        val today: LocalDate = LocalDate.now()
-        val birthday: LocalDate = LocalDate.parse("1998-06-09", formatter)
+        GlobalScope.launch {
 
-        val p: Period = Period.between(birthday, today)
+            useCaseBirthCheckUserBirthCache.invoke(Unit).collect { userBirthDateState ->
 
-        val currentClockTime = TimeUnit.HOURS.toMillis(SimpleDateFormat("HH").format(Date()).toLong()) +
-                TimeUnit.MINUTES.toMillis(SimpleDateFormat("mm").format(Date()).toLong()) +
-                TimeUnit.SECONDS.toMillis(SimpleDateFormat("ss").format(Date()).toLong())
+                when (userBirthDateState.status) {
 
+                    BirthResource.Status.REGISTERED -> {
+                        userBirthDateState.data?.let {
+                            remoteViews = calculateAndShowUserAge(remoteViews, it)
+                        } ?: run { remoteViews = showWeDontHaveYourBirthDate(remoteViews) }
+                    }
+                    BirthResource.Status.NOT_REGISTERED -> {
 
-        remoteViews.setTextViewText(R.id.text_view_exact_age, "${p.years}:${p.months}:${p.days}")
-        remoteViews.setChronometerCountDown(R.id.chronometer_age, false)
-        remoteViews.setChronometer(R.id.chronometer_age,SystemClock.elapsedRealtime() - currentClockTime , null , true)
+                        remoteViews = showWeDontHaveYourBirthDate(remoteViews)
+                    }
+                }
+            }
+
+        }
+
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
 
+    }
+
+    private fun calculateAndShowUserAge(
+        remoteViews: RemoteViews,
+        userBirthDomain: UserBirthDomain
+    ): RemoteViews {
+
+
+        val calculateAge =
+            timeManager.calculateAge(userBirthDomain.userBirthDomain.birthDateFormated)
+
+        remoteViews.setViewVisibility(R.id.text_view_exact_age, View.VISIBLE)
+        remoteViews.setViewVisibility(R.id.chronometer_age, View.VISIBLE)
+        remoteViews.setViewVisibility(R.id.text_view_we_dont_have_your_birth_date, View.GONE)
+        remoteViews.setTextViewText(
+            R.id.text_view_exact_age,
+            "${calculateAge.first.years}:${calculateAge.first.months}:${calculateAge.first.days}"
+        )
+        remoteViews.setChronometerCountDown(R.id.chronometer_age, false)
+        remoteViews.setChronometer(R.id.chronometer_age, calculateAge.second, null, true)
+        return remoteViews
+    }
+
+    private fun showWeDontHaveYourBirthDate(remoteViews: RemoteViews): RemoteViews {
+
+        remoteViews.setViewVisibility(R.id.text_view_exact_age, View.GONE)
+        remoteViews.setViewVisibility(R.id.chronometer_age, View.GONE)
+        remoteViews.setViewVisibility(R.id.text_view_we_dont_have_your_birth_date, View.VISIBLE)
+        return remoteViews
     }
 
     override fun onReceive(context: Context?, intent: Intent) {
         if (intent.action == ACTION_SCHEDULED_UPDATE) {
             val manager = AppWidgetManager.getInstance(context)
-            val ids = manager.getAppWidgetIds(context?.let { ComponentName(it, WidgetExactAge::class.java) })
+            val ids = manager.getAppWidgetIds(context?.let {
+                ComponentName(
+                    it,
+                    WidgetExactAge::class.java
+                )
+            })
             onUpdate(context!!, manager, ids)
         }
         super.onReceive(context, intent)

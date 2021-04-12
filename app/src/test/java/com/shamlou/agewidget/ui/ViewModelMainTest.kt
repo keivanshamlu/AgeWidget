5dc8c54c086263b8d195289582b3661aff91f22c
keivanshamlu <keivan.shamlu.co@gmail.com>
2021-04-12 23:47:37 +0430

    simple test for viewmodel live data

--- app/src/test/java/com/shamlou/agewidget/ui/ViewModelMainTest.kt
+++ app/src/test/java/com/shamlou/agewidget/ui/ViewModelMainTest.kt
@@ -1,10 +1,20 @@
 package com.shamlou.agewidget.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.shamlou.agewidget.MainCoroutineRule
+import com.shamlou.agewidget.getOrAwaitValue
+import com.shamlou.agewidget.manager.TimeManager
+import com.shamlou.agewidget.usecases.UseCaseBirthCheckUserBirthCache
+import com.shamlou.agewidget.usecases.UseCaseBirthInsertUserBirthCache
+import com.shamlou.agewidget.usecases.UseCaseDeleteUserBirthCache
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.hamcrest.Matchers.`is`
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -15,6 +25,27 @@ class ViewModelMainTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    @MockK
+    lateinit var useCaseCheckUserBirthCache: UseCaseBirthCheckUserBirthCache
+    @MockK
+    lateinit var useCaseInsertUserBirthCache: UseCaseBirthInsertUserBirthCache
+    @MockK
+    lateinit var useCaseDeleteUserBirthCache: UseCaseDeleteUserBirthCache
+    @MockK
+    lateinit var timeManager: TimeManager
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    lateinit var viewModelMain: ViewModelMain
+    @Before
+    fun setUp(){
+
+        MockKAnnotations.init(this)
+        viewModelMain = ViewModelMain(useCaseCheckUserBirthCache , useCaseInsertUserBirthCache , useCaseDeleteUserBirthCache , timeManager)
+    }
     @Test
     fun getUserBirthCache() {
 
@@ -87,6 +118,13 @@ class ViewModelMainTest {
 
     @Test
     fun deleteSelectedDate() {
+
+        //Given
+
+        //when
+        viewModelMain.deleteSelectedDate()
+        //then
+        assertThat(viewModelMain.mainPageStates.getOrAwaitValue() , `is`(MainPageStates.DATE_NOT_SELECTED))
     }
 
     @Test

237480d40fad7a4f586d8f03b46e5b0aeacad252
keivanshamlu <keivan.shamlu.co@gmail.com>
2021-04-13 20:23:11 +0430

    more on RepositoryBirthImplTest.kt

---
+++ app/src/test/java/com/shamlou/agewidget/repository/birth/RepositoryBirthFakeData.kt
@@ -0,0 +1,5 @@
+package com.shamlou.agewidget.repository.birth
+
+import com.shamlou.agewidget.db.user.UserBirth
+
+val fakeUserBirth = UserBirth(1 , "first_name" , "birth_day" , "birth_month" , "birth_year","birth_date_formated")
--- app/src/test/java/com/shamlou/agewidget/repository/birth/RepositoryBirthImplTest.kt
+++ app/src/test/java/com/shamlou/agewidget/repository/birth/RepositoryBirthImplTest.kt
@@ -5,6 +5,7 @@ import com.shamlou.agewidget.MainCoroutineRule
 import com.shamlou.agewidget.base.BirthResource
 import com.shamlou.agewidget.db.user.UserBirth
 import com.shamlou.agewidget.db.user.UserDao
+import com.shamlou.agewidget.domain.UserBirthDomain
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -44,13 +45,32 @@ class RepositoryBirthImplTest {
 
     @Test
     @ExperimentalCoroutinesApi
-    fun getUserBirth() = mainCoroutineRule.runBlockingTest {
+    fun getUserBirthSuccess() = mainCoroutineRule.runBlockingTest {
 
         //given
         val result = repository.getUserBirth()
 
         //when
-        every { userDao.getUserBirth() } returns UserBirth(1 , "keiavn" , "birthday" , "m" , "y","formatted")
+        every { userDao.getUserBirth() } returns fakeUserBirth
+
+        //then
+        val actual = mutableListOf<BirthResource<UserBirthDomain>>()
+        result.take(2).collect {
+            actual.add(it)
+        }
+        Assert.assertThat( actual[0].status , equalTo(BirthResource.Status.LOADING))
+        Assert.assertThat( actual[1].status , equalTo(BirthResource.Status.REGISTERED))
+        Assert.assertThat( actual[1].data?.firstName , equalTo(fakeUserBirth.firstName))
+    }
+    @Test
+    @ExperimentalCoroutinesApi
+    fun getUserBirthFailure() = mainCoroutineRule.runBlockingTest {
+
+        //given
+        val result = repository.getUserBirth()
+
+        //when
+        every { userDao.getUserBirth() } returns null
 
         //then
         val actual = mutableListOf<BirthResource.Status>()
@@ -58,7 +78,7 @@ class RepositoryBirthImplTest {
             actual.add(it.status)
         }
         Assert.assertThat( actual[0] , equalTo(BirthResource.Status.LOADING))
-        Assert.assertThat( actual[1] , equalTo(BirthResource.Status.REGISTERED))
+        Assert.assertThat( actual[1] , equalTo(BirthResource.Status.NOT_REGISTERED))
     }
 
     @Test
